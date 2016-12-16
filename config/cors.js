da7c4efe09c38abc00242a07e4b4cd33226b947e
emanueledivizio <emadivizio@gmail.com>
2016-12-16 22:09:38 +0100

    cors

--- config/cors.js
+++ config/cors.js
@@ -30,7 +30,7 @@ module.exports.cors = {
     
     allRoutes: true,
 origin: '*',
-    credentials: false
+    credentials: true
 
   /***************************************************************************
   *                                                                          *
